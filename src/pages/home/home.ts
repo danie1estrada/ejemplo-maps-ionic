ccb389a9ecf00347a9099d7839f2a5cda6f750e1
Daniel Estrada <isc.daniel.estrada@gmail.com>
2018-11-30 22:40:48 -0600

    Primer commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapRef: ElementRef;
+
+  private map: any;
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  ionViewDidLoad() {
+    this.showMap();
+  }
+
+  private showMap() {
+    const center = { lat: 21.109350, lng: -101.625594 };
+    const options = {
+      center,
+      zoom: 15,
+      disableDefaultUI: true,
+      styles: [
+        {
+          featureType: 'poi',
+          stylers: [{ visibility: 'off' }]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.icon',
+          stylers: [{ visibility: 'off' }]
+        }
+      ]
+    };
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  }
 }
